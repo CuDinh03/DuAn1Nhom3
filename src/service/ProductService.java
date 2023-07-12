0c44fb03b0beb01a4c77f04639e6aee5b0aeaa05
cudh <cudh@crmviet.vn>
2023-07-12 10:35:23 +0700

    clean project

--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package service;
 
 import DAO.ProductDAO;
 import java.util.List;
 import model.Product;
 
-/**
- *
- * @author maccuacu
- */
+
 public class ProductService implements IService<Product>{
     private ProductDAO prDao = new ProductDAO();
 
