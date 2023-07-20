c11579f09ee6e4dd856c61502f32e11074a498d0
cudh <cudh@crmviet.vn>
2023-07-20 12:42:39 +0700

    create Store Product

---
+++ src/model/StoreProducts.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author maccuacu
+ */
+public class StoreProducts {
+
+    private String id;
+    private String sanPhamId;
+    private String cuaHangId;
+    private String tenSp;
+    private int soLuong;
+    private Date ngayTao;
+    private Date ngaySua;
+    private int status;
+
+}
